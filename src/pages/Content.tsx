e87eeeba9252bfef909e0d16b1f240278bcaac68
Siddharth <165468666+sbharth1@users.noreply.github.com>
2025-03-11 05:15:45 +0530

    axios install to get and post apis

--- src/pages/Content.tsx
+++ src/pages/Content.tsx
@@ -1,15 +1,27 @@
+import {useEffect, useState} from 'react';
 import {Text, StyleSheet, View, ScrollView} from 'react-native';
 import {Card} from 'react-native-paper';
 
 const Content = () => {
+  const [cardData, setCardData] = useState([]);
+  useEffect(() => {
+    const setData = async () => {
+      fetch('https://67c937330acf98d0708941b3.mockapi.io/demo/fakedata')
+        .then(res => res.json())
+        .then(res => setCardData(res));
+    };
+    setData();
+  }, []);
   return (
     <ScrollView contentContainerStyle={styles.scrollViewContainer}>
       <View style={styles.card}>
-        <Card.Title
-          style={styles.maincard}
-          title="Card Title"
-          subtitle="Card Subtitle"
-        />
+        {cardData.map((val, i) => (
+          <Card.Title key={i}
+            style={styles.maincard}
+            title="Card Title"
+            subtitle="Card Subtitle"
+          />
+        ))}
       </View>
     </ScrollView>
   );
@@ -20,19 +32,19 @@ export default Content;
 const styles = StyleSheet.create({
   scrollViewContainer: {
     flexGrow: 1,
-    justifyContent: 'flex-start', 
+    justifyContent: 'flex-start',
     alignItems: 'center',
-    paddingTop: 20, 
+    paddingTop: 20,
   },
   card: {
-    backgroundColor: 'green',
     display: 'flex',
     justifyContent: 'center',
-    alignItems: 'center', 
+    alignItems: 'center',
     marginBottom: 20,
   },
   maincard: {
     width: '80%',
     backgroundColor: 'red',
+    margin:5,
   },
 });
